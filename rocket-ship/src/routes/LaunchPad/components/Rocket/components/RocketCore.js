58176734965f2d320472fac527af6c877e71bce8
JhamG9 <jhampiergiron@gmail.com>
2021-07-05 07:11:45 -0500

    refactor(rocket-ship/src/routes/LaunchPad/components/): changed variables for rocket don't fly

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/RocketCore.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/RocketCore.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/_rocket.scss';
 
-const SECONDS_TO_TAKEOFF = 5;
+const SECONDS_TO_TAKEOFF = 0;
 const MS_TO_TAKEOFF = SECONDS_TO_TAKEOFF * 1000;
-const FINAL_POSITION_BOTTOM_VAL = 'calc(400px)';
+const FINAL_POSITION_BOTTOM_VAL = 'calc(0px)';
 
 function timeToPositionPercent(startTime) {
   const now = Date.now();
